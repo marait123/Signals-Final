c39dad688748adbad23ef8e55207ca40cd89fb57
m_abobakr <m.abobakr10@yahoo.com>
2020-05-13 01:59:29 +0200

    Added a DCT compressor function

--- 3rd-task/utils.py
+++ 3rd-task/utils.py
@@ -1,5 +1,9 @@
+from scipy.fft import dct
 import cv2
-RGB = {'blue': 0, 'green':1, 'red':2}
+import numpy as np
+
+RGB = {'blue': 0, 'green': 1, 'red': 2}
+
 
 def getImageComponent(compName, imageMat):
     """
@@ -10,10 +14,49 @@ def getImageComponent(compName, imageMat):
     for i in range(3):
         if (i == RGB[compName.lower()]):
             continue
-        b[:,:,i] = 0    
+        b[:, :, i] = 0
     return b
-    
+
+
 def imgDisplay(imageName, imageMat):
-    cv2.imshow(imageName,imageMat)
+    cv2.imshow(imageName, imageMat)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+# A function that calculates the dct transform of a given matrix
+def blockDct(block):
+    return dct(dct(block.T, norm='ortho').T, norm='ortho')
+
+# A function that compresses images
+def compress(image, m):
+    # Extract the number of columns and rows from the dimensions of the image
+    rows, cols, = image.shape[0:2]
+
+    # Calculating the dimensions of the new array and initialzing it
+    newrows = rows / 8 * m
+    newcols = cols / 8 * m
+    newImage = np.zeros((int(newrows), int(newcols), 3))
+
+    # Changing range of values from 0-255 to (-128)-127
+    image = image.astype('int16') - 128
+
+    # Calculating DCT for the image in place
+    # we calculate the DCT by slicing an 8x8 matrix and calculate its DCT matrix
+    for i in range(0, rows, 8):
+        for j in range(0, cols, 8):
+            for k in range(3):
+                image[i:i+8, j:j+8, k] = blockDct(image[i:i+8, j:j+8, k])
+
+  # Slicing the top left mxm matrix from each block
+    for i in range(0, int(rows/8)):
+        for j in range(0, int(cols/8)):
+            for k in range(3):
+                newy = i*m
+                newx = j*m
+                oldx = j*8
+                oldy = i*8
+                newImage[newy:newy+m, newx:newx+m,
+                         k] = image[oldy:oldy+m, oldx:oldx+m, k]
+    return newImage
+
